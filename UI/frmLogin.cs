6a1beceab369344fa526d7a059a64f30e24d084e
JcMirandaL <juliomiranda271@gmail.com>
2025-09-02 08:19:08 -0600

    Actualizo form recuperar contra

--- UI/frmLogin.cs
+++ UI/frmLogin.cs
@@ -21,5 +21,30 @@ namespace UI
         {
 
         }
+<<<<<<< Updated upstream
+=======
+
+
+        //evento click del boton cancelar
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            //aqui cierro el formulario
+            this.Close();
+        }
+
+
+        //evento linkclicked del linklabel olvido contraseña
+        private void linklblOlvidoContra_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            //muestro el formulario de recuperar contraseña
+            frmRecuperarContra frmRecuperar = new frmRecuperarContra();
+            frmRecuperar.ShowDialog();
+            //esto hace que el formulario de login se oculte mientras se muestra el de recuperar contraseña
+            this.Show();
+            //cuando se cierre el formulario de recuperar contraseña, el de login se vuelve a mostrar
+            this.Hide();
+
+        }
+>>>>>>> Stashed changes
     }
 }
