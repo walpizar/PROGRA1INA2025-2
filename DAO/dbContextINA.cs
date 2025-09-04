110d21e8f53cf34af6e2fbc57a8c67f28aaa0314
DenBrais <denisbrais2005@gmail.com>
2025-09-04 08:34:15 -0600

    entidades agregadas alcontexto

--- DAO/dbContextINA.cs
+++ DAO/dbContextINA.cs
@@ -10,17 +10,57 @@ namespace DAO
 {
     internal class dbContextINA: DbContext
     {
-        //definir la entidades de dominio que desea mapear a la base de datos
-        public DbSet<clsActivos> Activos { get; set; }
-        public DbSet<clsCategoriaActivos> CategoriaActivos { get; set; }
-        public DbSet<clsPersona> Personas { get; set; }
-        public DbSet<clsMedico> Medicos { get; set; }
-        public DbSet<clsDepartamentos> Departamentos { get; set; }
-        public DbSet<clsDevolucion> Devoluciones { get; set; }
-        public DbSet<clsEnfermero> Enfermeros { get; set; }
-        public DbSet<clsEspecialidadMedica> EspecialidadesMedicas { get; set; }
-        public DbSet<clsPuestos> Puestos { get; set; }
+        // Entidades mapeadas
+        public DbSet<clsPersona> persona { get; set; }
+        public DbSet<clsDonante> donante { get; set; }
+        public DbSet<clsDonacion> donacion { get; set; }
+        public DbSet<clsDonacionDinero> donacionDinero { get; set; }
+        public DbSet<clsActivos> activos { get; set; }
+        public DbSet<clsDonacionesActivos> donacionesActivos { get; set; }
+        public DbSet<clsCategoria> categoria { get; set; }
+        public DbSet<clsCategoriaActivos> categoriaActivos { get; set; }
+        public DbSet<clsDepartamentos> departamentos { get; set; }
+        public DbSet<clsDevolucion> devolucion { get; set; }
+        public DbSet<clsEnfermero> enfermero { get; set; }
+        public DbSet<clsEspecialidadMedica> especialidadMedica { get; set; }
+        public DbSet<clsMedico> medico { get; set; }
+        public DbSet<clsPermisos> permisos { get; set; }
+        public DbSet<clsProducto> producto { get; set; }
+        public DbSet<clsPuestos> puestos { get; set; }
+        public DbSet<clsRol> rol { get; set; }
+        public DbSet<clsRolPermiso> rolPermiso { get; set; }
+        public DbSet<clsUsuario> usuario { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Clave primaria compuesta para Persona
+            modelBuilder.Entity<clsPersona>()
+                .HasKey(p => new { p.id, p.tipoId });
+            modelBuilder.Entity<clsPersona>().Property(p => p.id)
+                .IsRequired().HasMaxLength(20).ValueGeneratedNever();
+            modelBuilder.Entity<clsPersona>().Property(p => p.tipoId)
+                .IsRequired().ValueGeneratedNever();
+
+            // Clave primaria compuesta para Donante
+            modelBuilder.Entity<clsDonante>()
+                .HasKey(d => new { d.idDonante, d.idPersona });
+
+            // Relación 1 a 1 entre Donante y Persona
+            modelBuilder.Entity<clsDonante>()
+                .HasOne(d => d.persona)
+                .WithOne(p => p.donante)
+                .HasForeignKey<clsDonante>(d => new { d.idDonante, d.idPersona });
+
+            // Clave primaria compuesta para Enfermero
+            modelBuilder.Entity<clsEnfermero>()
+                .HasKey(e => new { e.id, e.tipoId });
+
+            // Clave primaria compuesta para RolPermiso
+            modelBuilder.Entity<clsRolPermiso>()
+                .HasKey(rp => new { rp.idRol, rp.idPermiso });
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -31,44 +71,5 @@ namespace DAO
                     @"Server=localhost\sqlexpress;Database=dbPaleativoGarabito;Trusted_Connection=True;MultipleActiveResultSets=True;TrustServerCertificate=True;");
             }
         }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Configuración para tbPersonas
-            modelBuilder.Entity<clsPersona>().HasKey(p => new { p.id, p.tipoId });
-
-            modelBuilder.Entity<clsPersona>().Property(p => p.id)
-                .IsRequired()
-                .HasMaxLength(20)
-                .ValueGeneratedNever();
-
-            modelBuilder.Entity<clsPersona>().Property(p => p.tipoId)
-                .IsRequired()
-                .ValueGeneratedNever();
-
-            //clsMedico configuracion de llave primaria compuesta   
-            modelBuilder.Entity<clsMedico>().HasKey(m => new { m.id, m.tipoId });
-            modelBuilder.Entity<clsMedico>().Property(m => m.id)
-                .IsRequired()
-                .HasMaxLength(20)
-                .ValueGeneratedNever();
-            modelBuilder.Entity<clsMedico>().Property(m => m.tipoId).IsRequired()
-                .ValueGeneratedNever();
-
-            //relacion 1 a 1 entre medico y persona
-
-            modelBuilder.Entity<clsMedico>()
-                .HasOne(m => m.persona)
-                .WithOne()
-                .HasForeignKey<clsMedico>(m => new { m.id, m.tipoId })
-                .OnDelete(DeleteBehavior.Restrict); // Evita el borrado en cascada
-
-
-
-
-        }
-
     }
 }
