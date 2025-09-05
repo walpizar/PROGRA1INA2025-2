99ca37df93cb2b29ad94b155f8e9e34b32d2cdc0
Meyling Rojas <mpatric03@hotmail.com>
2025-09-05 12:21:16 -0600

    Se agregaron las clases de UsuarioDAO y el servicio de Usuario

---
+++ DAO/UsuarioDAO.cs
@@ -0,0 +1,69 @@
+﻿using Common.Interfaces;
+using Entities;
+
+namespace DAO
+{
+    public class UsuarioDAO : IGenerica<clsUsuario, string>
+    {
+        private dbContextINA _context;
+
+        public UsuarioDAO() { 
+            _context = new dbContextINA();
+
+        }
+
+        public void crear(clsUsuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+        }
+
+        public void modificar(clsUsuario usuario)
+        {
+            _context.Usuarios.Update(usuario);
+            _context.SaveChanges();
+
+        }
+
+        public void eliminar(string id)
+        {
+            // Se busca el usuario primero para evitar un error de referencia nula
+            var usua = _context.Usuarios.SingleOrDefault(u => u.id == id);
+            if (usua != null)
+            {
+                _context.Usuarios.Remove(usua);
+                _context.SaveChanges();
+
+            }
+
+        }
+        public clsUsuario consultarPorID(string id)
+        {
+            return _context.Usuarios.Find(id);
+        }
+
+        public clsUsuario consultarPorID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public clsUsuario consultarPorNombre(string nombre)
+        {
+            return _context.Usuarios.Where(u => u.nombre_usuario.Trim().ToUpper()
+                                              == nombre.Trim().ToUpper()).SingleOrDefault();
+        }
+
+        public List<clsUsuario> consultarTodos()
+        {
+            return _context.Usuarios.ToList();
+
+        }
+
+        public void eliminar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
---
+++ Services/Usuario.cs
@@ -0,0 +1,61 @@
+﻿using Common.Interfaces;
+using DAO;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class UsuarioService : IGenerica<clsUsuario, string>
+    {
+      
+        UsuarioDAO _usuarioDAO= new UsuarioDAO();
+        public UsuarioService() { }
+
+
+        public void crear(clsUsuario usuario)
+        {
+            // regla de negocio valida queno exista un Usuario con el mismo id
+            if (_usuarioDAO.consultarPorID(usuario.id) != null)
+            {
+                throw new Exception("El cliente ya existe");
+            }
+            _usuarioDAO.crear(usuario);
+        }
+
+        public void modificar(clsUsuario usuario)
+        {
+            _usuarioDAO.modificar(usuario);
+        }
+
+        public void eliminar(string id)
+        {
+            //validar que el usuario exista
+            if (_usuarioDAO.consultarPorID(id) == null)
+            {
+                throw new Exception("El usuario no existe");
+            }
+
+            _usuarioDAO.eliminar(id);
+        }
+
+        public clsUsuario consultarPorID(string id)
+        {
+            return _usuarioDAO.consultarPorID(id);
+        }
+
+        public clsUsuario consultarPorNombre(string nombre)
+        {
+            return _usuarioDAO.consultarPorNombre(nombre);
+        }
+
+        public List<clsUsuario> consultarTodos()
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
