83172772ae006e983083bbedf6ef44e87bbf360e
MarcoMedina-git <markosmedinaseas6@gmail.com>
2025-09-16 17:35:21 -0600

    Reparado conflictos

--- UI/Program.cs
+++ UI/Program.cs
@@ -23,7 +23,7 @@ namespace UI
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
-        Application.Run(new FrmAnadirAdmin());
+        Application.Run(new frmPrincipal());
 
         
         Application.Run(new frmActivos());
