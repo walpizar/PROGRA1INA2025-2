345e6622b84ab10f6dc2efbd7b55a84868e54bba
Agustin <atte.agus0418@gmail.com>
2025-09-23 16:02:25 -0600

    Merge de develop en feature/menuprincipalagustin

--- DAO/dbContextINA.cs
+++ DAO/dbContextINA.cs
@@ -20,6 +20,10 @@ namespace DAO
         public DbSet<clsRolPermiso> rolPermiso { get; set; }
         public DbSet<clsUsuario> usuario { get; set; }
         public DbSet<clsModulo> modulos { get; set; }
+        public DbSet<clsPaciente> paciente { get; set; }
+        //public DbSet<clsDonante> donante { get; set; }
+        //public DbSet<clsDonacion> donacion { get; set; }
+        public DbSet<clsCategoriaActivos> categoriaActivos { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -49,7 +53,7 @@ namespace DAO
                 .HasKey(u => new { u.personaId, u.personaTipoId });
 
             modelBuilder.Entity<clsUsuario>()
-                .HasOne(u => u.persona)
+                .HasOne(u => u.persona)//Establece la relación de 1 a 1(un usuario tiene una persona)
                 .WithOne()
                 .HasForeignKey<clsUsuario>(u => new { u.personaId, u.personaTipoId })
                 .HasPrincipalKey<clsPersona>(p => new { p.id, p.tipoId });
@@ -57,21 +61,61 @@ namespace DAO
             // Medico
             modelBuilder.Entity<clsMedico>().HasKey(m => new { m.id, m.tipoId });
             modelBuilder.Entity<clsMedico>().Property(m => m.id)
-                .IsRequired().HasMaxLength(20).ValueGeneratedNever();
+                .IsRequired()
+                .HasMaxLength(20)        // use esto solo si 'id' es string
+                .ValueGeneratedNever();
+
             modelBuilder.Entity<clsMedico>().Property(m => m.tipoId)
-                .IsRequired().ValueGeneratedNever();
+                .IsRequired()
+                .ValueGeneratedNever();  // no ponga HasMaxLength si 'tipoId' es int
+
 
             modelBuilder.Entity<clsMedico>()
                 .HasOne(m => m.persona)
                 .WithOne()
                 .HasForeignKey<clsMedico>(m => new { m.id, m.tipoId })
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Restrict); // Evita el borrado en cascada
 
-            // Enfermero
+
+            /*------------------------------------------------------------*/
+            //clsPaciente configuracion de llave primaria compuesta
+            modelBuilder.Entity<clsPaciente>().HasKey(p => new { p.id, p.tipoId });
+
+            //configuro las propiedades de la llave primaria compuesta para id
+            modelBuilder.Entity<clsPaciente>().Property(p => p.id)
+                .IsRequired()//obligatorio
+                .HasMaxLength(20)//longitud maxima
+                .ValueGeneratedNever();//no se genera automaticamente
+
+            //configuro las propiedades de la llave primaria compuesta para tipoId
+            modelBuilder.Entity<clsPaciente>().Property(p => p.tipoId)
+                .IsRequired()
+                .ValueGeneratedNever();
+
+            //relacion 1 a 1 entre paciente y persona
+            modelBuilder.Entity<clsPaciente>()
+                .HasOne(p => p.persona)//navegacion desde paciente a persona
+                .WithOne(per => per.paciente)//relacion 1 a 1 con persona
+                .HasForeignKey<clsPaciente>(p => new { p.id, p.tipoId })//FK en paciente
+                .HasPrincipalKey<clsPersona>(per => new { per.id, per.tipoId })//PK en persona que es la misma que la FK en paciente
+                .OnDelete(DeleteBehavior.Restrict); // Evita el borrado en cascada
+            /*------------------------------------------------------------*/
+
+
+            // RolPermiso
+
+
+            //// Relación 1 a 1 entre Donante y Persona
+            //modelBuilder.Entity<clsDonante>()
+            //    .HasOne(d => d.persona)
+            //    .WithOne(p => p.donante)
+            //    .HasForeignKey<clsDonante>(d => new { d.personaId, d.personaTipoId });
+
+            // Clave primaria compuesta para Enfermero
             modelBuilder.Entity<clsEnfermero>()
                 .HasKey(e => new { e.id, e.tipoId });
 
-            // RolPermiso
+            // Clave primaria compuesta para RolPermiso
             modelBuilder.Entity<clsRolPermiso>()
                 .HasKey(rp => new { rp.idRol, rp.idPermiso });
         }
---
+++ UI/frmPacienteLista.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace UI
+{
+    partial class frmPacienteLista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnNuevo = new Button();
+            gbxListaPacientes = new GroupBox();
+            rdBtnInactivos = new RadioButton();
+            rdBtnActivos = new RadioButton();
+            lblEstadoPacientes = new Label();
+            lblBusqueda = new Label();
+            txtBusqueda = new TextBox();
+            lstvListaPaciente = new ListView();
+            colId = new ColumnHeader();
+            colNombre = new ColumnHeader();
+            colApellido1 = new ColumnHeader();
+            colApellido2 = new ColumnHeader();
+            colTelefono = new ColumnHeader();
+            lblTitulo = new Label();
+            gbxListaPacientes.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnNuevo
+            // 
+            btnNuevo.FlatAppearance.BorderColor = Color.Red;
+            btnNuevo.FlatAppearance.BorderSize = 3;
+            btnNuevo.Location = new Point(759, 30);
+            btnNuevo.Name = "btnNuevo";
+            btnNuevo.Size = new Size(120, 40);
+            btnNuevo.TabIndex = 1;
+            btnNuevo.Text = " Nuevo";
+            btnNuevo.UseVisualStyleBackColor = true;
+            btnNuevo.Click += btnNuevo_Click;
+            // 
+            // gbxListaPacientes
+            // 
+            gbxListaPacientes.Controls.Add(rdBtnInactivos);
+            gbxListaPacientes.Controls.Add(rdBtnActivos);
+            gbxListaPacientes.Controls.Add(lblEstadoPacientes);
+            gbxListaPacientes.Controls.Add(lblBusqueda);
+            gbxListaPacientes.Controls.Add(txtBusqueda);
+            gbxListaPacientes.Controls.Add(lstvListaPaciente);
+            gbxListaPacientes.Location = new Point(14, 76);
+            gbxListaPacientes.Name = "gbxListaPacientes";
+            gbxListaPacientes.Size = new Size(865, 498);
+            gbxListaPacientes.TabIndex = 5;
+            gbxListaPacientes.TabStop = false;
+            gbxListaPacientes.Text = "Pacientes";
+            // 
+            // rdBtnInactivos
+            // 
+            rdBtnInactivos.AutoSize = true;
+            rdBtnInactivos.Location = new Point(721, 50);
+            rdBtnInactivos.Name = "rdBtnInactivos";
+            rdBtnInactivos.Size = new Size(72, 19);
+            rdBtnInactivos.TabIndex = 4;
+            rdBtnInactivos.TabStop = true;
+            rdBtnInactivos.Text = "Inactivos";
+            rdBtnInactivos.UseVisualStyleBackColor = true;
+            rdBtnInactivos.CheckedChanged += rdBtnInactivos_CheckedChanged;
+            // 
+            // rdBtnActivos
+            // 
+            rdBtnActivos.AutoSize = true;
+            rdBtnActivos.Location = new Point(640, 50);
+            rdBtnActivos.Name = "rdBtnActivos";
+            rdBtnActivos.Size = new Size(64, 19);
+            rdBtnActivos.TabIndex = 8;
+            rdBtnActivos.TabStop = true;
+            rdBtnActivos.Text = "Activos";
+            rdBtnActivos.UseVisualStyleBackColor = true;
+            rdBtnActivos.CheckedChanged += rdBtnActivos_CheckedChanged;
+            // 
+            // lblEstadoPacientes
+            // 
+            lblEstadoPacientes.AutoSize = true;
+            lblEstadoPacientes.Location = new Point(669, 27);
+            lblEstadoPacientes.Name = "lblEstadoPacientes";
+            lblEstadoPacientes.Size = new Size(100, 15);
+            lblEstadoPacientes.TabIndex = 7;
+            lblEstadoPacientes.Text = "Lista de Pacientes";
+            // 
+            // lblBusqueda
+            // 
+            lblBusqueda.AutoSize = true;
+            lblBusqueda.Location = new Point(19, 27);
+            lblBusqueda.Name = "lblBusqueda";
+            lblBusqueda.Size = new Size(62, 15);
+            lblBusqueda.TabIndex = 4;
+            lblBusqueda.Text = "Busqueda:";
+            // 
+            // txtBusqueda
+            // 
+            txtBusqueda.Location = new Point(18, 50);
+            txtBusqueda.Name = "txtBusqueda";
+            txtBusqueda.Size = new Size(554, 23);
+            txtBusqueda.TabIndex = 2;
+            txtBusqueda.TextChanged += txtBusqueda_TextChanged;
+            // 
+            // lstvListaPaciente
+            // 
+            lstvListaPaciente.Columns.AddRange(new ColumnHeader[] { colId, colNombre, colApellido1, colApellido2, colTelefono });
+            lstvListaPaciente.FullRowSelect = true;
+            lstvListaPaciente.GridLines = true;
+            lstvListaPaciente.Location = new Point(15, 88);
+            lstvListaPaciente.MultiSelect = false;
+            lstvListaPaciente.Name = "lstvListaPaciente";
+            lstvListaPaciente.Size = new Size(838, 394);
+            lstvListaPaciente.TabIndex = 3;
+            lstvListaPaciente.UseCompatibleStateImageBehavior = false;
+            lstvListaPaciente.View = View.Details;
+            lstvListaPaciente.MouseDoubleClick += lstvListaPaciente_MouseDoubleClick;
+            // 
+            // colId
+            // 
+            colId.Text = "ID";
+            colId.Width = 80;
+            // 
+            // colNombre
+            // 
+            colNombre.Text = "Nombre";
+            colNombre.Width = 200;
+            // 
+            // colApellido1
+            // 
+            colApellido1.Text = "Apellido 1";
+            colApellido1.Width = 200;
+            // 
+            // colApellido2
+            // 
+            colApellido2.Text = "Apellido 2";
+            colApellido2.Width = 200;
+            // 
+            // colTelefono
+            // 
+            colTelefono.Text = "Telefono";
+            colTelefono.Width = 150;
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.AutoSize = true;
+            lblTitulo.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitulo.Location = new Point(311, 30);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(262, 30);
+            lblTitulo.TabIndex = 3;
+            lblTitulo.Text = "Mantenimiento Pacientes";
+            // 
+            // frmPacienteLista
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(895, 586);
+            Controls.Add(btnNuevo);
+            Controls.Add(gbxListaPacientes);
+            Controls.Add(lblTitulo);
+            Name = "frmPacienteLista";
+            Text = "Mantenimiento: Pacientes";
+            Load += frmPacienteLista_Load;
+            gbxListaPacientes.ResumeLayout(false);
+            gbxListaPacientes.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnNuevo;
+        private GroupBox gbxListaPacientes;
+        private Label lblBusqueda;
+        private TextBox txtBusqueda;
+        private ListView lstvListaPaciente;
+        private ColumnHeader colId;
+        private ColumnHeader colNombre;
+        private ColumnHeader colApellido1;
+        private Label lblTitulo;
+        private ColumnHeader colApellido2;
+        private ColumnHeader colTelefono;
+        private Label lblEstadoPacientes;
+        private RadioButton rdBtnInactivos;
+        private RadioButton rdBtnActivos;
+    }
+}
